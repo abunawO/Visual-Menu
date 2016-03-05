6cae5d5bb0d25b2fbce76e2579f9ddf559695f1c
Abunaw Ose <aose@ggc.eud>
2016-03-05 01:28:12 +0000

    finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
+  #has a corespoding view with .html and .erb extansions
   def home
+    #render(text: "Home method in StaticPages controller class");
   end
-
+  #has a corespoding view with .html and .erb extansions
   def help
+    #render(text: "Help method in StaticPages controller class");
+  end
+  
+  def about
   end
 end
