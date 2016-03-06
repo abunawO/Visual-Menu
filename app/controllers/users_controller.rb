62202cd94e44b771bc8aba551332e1087143d319
Abunaw Ose <aose@ggc.eud>
2016-03-06 00:55:34 +0000

    added functioning user sing up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,30 @@
 class UsersController < ApplicationController
   def new
     #render(text:"hello i am user");
+    #@user = User.find(params[:id])
+    @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!" #Adding a flash message to user signup.
+      redirect_to @user #redirect to users page
+    else
+      render 'new'
+    end
+  end
+
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,10 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   #get 'static_pages/contact'
   get 'contact' => 'static_pages#contact'
+  
   get 'signup' => 'users#new'
 
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
