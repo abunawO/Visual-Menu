83cfd2272f2c875d8b88aed71006e4dd7f9d5646
Abunaw Ose <abunawose@gmail.com>
2018-12-29 09:06:24 -0500

    update

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
 
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "Vizhooals App"
   end
 
   test "should get home" do
@@ -23,7 +23,7 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
-  
+
   test "should get contact" do
     get :contact
     assert_response :success
