6b4d97b3d84d866fb20ed71d45da591917595344
Abunaw Ose <abunawose@gmail.com>
2019-01-09 18:56:00 -0500

    WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id] || params[:user_id]) || current_user
     @microposts = @user.microposts.paginate(page: params[:page])
     #binding.pry
+    @categories = []
     @categoriesPresnt = Micropost.where(:user_id => @user.id).select(:category).map(&:category).compact.present?
     return unless @categoriesPresnt
     @categories = Micropost.where(:user_id => @user.id).select(:category)&.distinct.select { |e| e.category.present? } || nil
