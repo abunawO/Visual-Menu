66534e79e9bb22eaa5be79e533ccc811444db4cd
Abunaw Ose <abunawose@gmail.com>
2019-02-02 13:18:10 -0500

    wip

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,9 @@
 class SessionsController < ApplicationController
 
   def new
-    #binding.pry
   end
 
  def create
-   binding.pry
     user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params[:password])
       if user.activated?
