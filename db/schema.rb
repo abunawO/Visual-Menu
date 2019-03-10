e7f535d5dba21c64b7baa0d7acaf19b5c47e29c6
Abunaw Ose <abunawose@gmail.com>
2019-03-10 11:43:12 -0400

    added links to support facebook and instagram

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190227193908) do
+ActiveRecord::Schema.define(version: 20190310153950) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 20190227193908) do
     t.string   "doordash_link"
     t.string   "grubhub_link"
     t.string   "menu_categories"
+    t.string   "facebook_link"
+    t.string   "instagram_link"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
