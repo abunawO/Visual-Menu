f42227a64cf568b560c182bacd53c11306281bfc
Abunaw Ose <abunawose@gmail.com>
2019-04-21 10:12:58 -0400

    fix bug

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,8 +4,8 @@ module UsersHelper
   def gravatar_for(user, options = { size: 170 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image = user.picture || gravatar_url
+    #gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image = user.picture
     image_tag(image, alt: user.name, class: "gravatar")
   end
 
